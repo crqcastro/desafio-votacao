317140de80f0e7fc5111a8dae350def596ba9c55
Cesar Castro <cesar.castro@grupomateus.com>
2024-11-06 13:52:42 -0300

    feat(Configuracao) Configuracao inicial do projeto

---
+++ run.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+PROFILE="local"
+DIR=${1:-"./docs"}
+REGEX=${2:-".*-$PROFILE\.env$"}
+
+ENV_FILE=$(find "$DIR" -maxdepth 1 -type f -regextype posix-extended -regex ".*/$REGEX" | head -n 1)
+
+if [ -z "$ENV_FILE" ]; then
+    echo "Nenhum arquivo .env encontrado com o padrão '$REGEX' no diretório '$DIR'."
+    exit 1
+fi
+
+export $(grep -v '^#' "$ENV_FILE" | xargs)
+echo "Variáveis de ambiente carregadas a partir do arquivo $ENV_FILE"
+
+
+mvn clean package -DskipTests
+cp ./target/votacao-*.jar ./target/votacao.jar
+sudo docker compose up -d
+java -jar ./target/votacao.jar --spring.profiles.active=$PROFILE
