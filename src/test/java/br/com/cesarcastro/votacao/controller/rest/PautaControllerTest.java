c8f8c01791305d9bd91a71bcf69c9ea0b054108b
Cesar Castro <cesarrqc@gmail.com>
2024-11-07 09:13:42 -0300

    feat[Pauta] consulta de pauta por id

--- src/test/java/br/com/cesarcastro/votacao/controller/rest/PautaControllerTest.java
+++ src/test/java/br/com/cesarcastro/votacao/controller/rest/PautaControllerTest.java
@@ -1,6 +1,11 @@
 package br.com.cesarcastro.votacao.controller.rest;
 
 import br.com.cesarcastro.votacao.domain.model.requests.PautaRequest;
+import br.com.cesarcastro.votacao.domain.model.responses.PautaResponse;
+import br.com.cesarcastro.votacao.domain.service.pauta.PautaService;
+import br.com.cesarcastro.votacao.mappers.PautaMapper;
+import br.com.cesarcastro.votacao.mappers.PautaMapperImpl;
+import br.com.cesarcastro.votacao.support.exceptions.RecursoNaoEncontradoException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.github.cdimascio.dotenv.Dotenv;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,11 +14,16 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.time.LocalDateTime;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -24,6 +34,12 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 class PautaControllerTest {
 
     private static final String URL = "/v1/pauta";
+
+    @MockBean
+    private PautaService pautaService;
+
+    @MockBean
+    private PautaMapper pautaMapper;
 
     @Autowired
     private MockMvc mockMvc;
@@ -60,12 +76,95 @@ class PautaControllerTest {
         pautaRequest.setDataHoraInicio(dataHoraInicio);
         pautaRequest.setDataHoraFim(dataHoraFim);
 
+        PautaResponse pautaResponse = new PautaResponse(1L,
+                "Pauta Teste",
+                "Nome da Pauta",
+                null,
+                null,
+                null,
+                null,
+                null);
+
+        given(pautaService.cadastrarPauta(any())).willReturn(pautaResponse);
+
         mockMvc.perform(post(URL)
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(pautaRequest)))
-                .andExpect(status().isCreated()) // Verifica se a resposta foi OK
-                .andExpect(jsonPath("$.id").value(1)) // Verifica o ID retornado
-                .andExpect(jsonPath("$.descricao").value("Pauta Teste")) // Verifica a descrição
-                .andExpect(jsonPath("$.nome").value("Nome da Pauta")); // Verifica o nome
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.id").value(1))
+                .andExpect(jsonPath("$.descricao").value("Pauta Teste"))
+                .andExpect(jsonPath("$.nome").value("Nome da Pauta"));
     }
+
+    @Test
+    public void testDeveReceberBadRequestAoCadastrarPautaComDataInferiorAAtual() throws Exception {
+        LocalDateTime dataHoraInicio = LocalDateTime.now().minusDays(1);
+        LocalDateTime dataHoraFim = LocalDateTime.now().plusDays(2);
+
+        PautaRequest pautaRequest = new PautaRequest();
+        pautaRequest.setDescricao("Pauta Teste");
+        pautaRequest.setNome("Nome da Pauta");
+        pautaRequest.setDataHoraInicio(dataHoraInicio);
+        pautaRequest.setDataHoraFim(dataHoraFim);
+
+        given(pautaService.cadastrarPauta(any())).willCallRealMethod();
+
+        mockMvc.perform(post(URL)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(pautaRequest)))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.message").value("Data de início da pauta não pode ser menor que a data atual"));
+    }
+
+    @Test
+    public void testDeveReceberBadRequestAoCadastrarPautaComDataFinalMenorQueInicial() throws Exception {
+        LocalDateTime dataHoraInicio = LocalDateTime.now().plusDays(1);
+        LocalDateTime dataHoraFim = LocalDateTime.now();
+
+        PautaRequest pautaRequest = new PautaRequest();
+        pautaRequest.setDescricao("Pauta Teste");
+        pautaRequest.setNome("Nome da Pauta");
+        pautaRequest.setDataHoraInicio(dataHoraInicio);
+        pautaRequest.setDataHoraFim(dataHoraFim);
+
+        given(pautaService.cadastrarPauta(any())).willCallRealMethod();
+
+        mockMvc.perform(post(URL)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(pautaRequest)))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.message").value("Data de fim da pauta não pode ser menor que a data de início"));
+    }
+
+    @Test
+    public void testDeveConsultarPautaPorIdComSucesso() throws Exception {
+
+        PautaResponse pautaMock = new PautaResponse(1L,
+                "Pauta Teste",
+                "Nome da Pauta",
+                null,
+                null,
+                null,
+                null,
+                null);
+
+        given(pautaService.consultarPautaPorId(1L)).willReturn(pautaMock);
+
+        mockMvc.perform(get(URL + "/1"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(pautaMock.id()))
+                .andExpect(jsonPath("$.descricao").value(pautaMock.descricao()))
+                .andExpect(jsonPath("$.nome").value(pautaMock.nome()));
+    }
+
+    @Test
+    public void testDeveConsultarPautaPorIdRecebendoNotFound() throws Exception {
+
+        given(pautaService.consultarPautaPorId(1L)).willThrow(new RecursoNaoEncontradoException("Pauta não encontrada"));
+
+        mockMvc.perform(get(URL + "/1"))
+                .andExpect(status().isNotFound())
+                .andExpect(jsonPath("$.message").value("Pauta não encontrada"));
+    }
+
 }
--- src/test/java/br/com/cesarcastro/votacao/domain/service/pauta/PautaServiceTest.java
+++ src/test/java/br/com/cesarcastro/votacao/domain/service/pauta/PautaServiceTest.java
@@ -5,12 +5,19 @@ import br.com.cesarcastro.votacao.domain.model.requests.PautaRequest;
 import br.com.cesarcastro.votacao.domain.model.responses.PautaResponse;
 import br.com.cesarcastro.votacao.domain.repositories.PautaRepository;
 import br.com.cesarcastro.votacao.mappers.PautaMapper;
+import br.com.cesarcastro.votacao.support.exceptions.BusinessException;
+import br.com.cesarcastro.votacao.support.exceptions.RecursoNaoEncontradoException;
 import br.com.cesarcastro.votacao.util.TestUtils;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -30,12 +37,50 @@ public class PautaServiceTest {
 
     @Test
     public void testCadastrarPauta() {
+        LocalDateTime dataHoraInicio = LocalDateTime.now().plusDays(1);
+        LocalDateTime dataHoraFim = LocalDateTime.now().plusDays(2);
         PautaRequest pautaRequest = TestUtils.generateRandom(PautaRequest.class);
+        pautaRequest.setDataHoraInicio(dataHoraInicio);
+        pautaRequest.setDataHoraFim(dataHoraFim);
         PautaEntity pautaEntity = this.mapper.toPautaEntity(pautaRequest);
         pautaEntity.setId(1L);
         when(pautaRepository.save(any())).thenAnswer(invocation -> invocation.getArgument(0));
         PautaResponse pautaResponse = this.pautaService.cadastrarPauta(pautaRequest);
 
         verify(pautaRepository).save(any());
+    }
+
+    @Test
+    public void deveLancarBusinessExceptionQuandoDataHoraInicioForMenorQueDataAtual() {
+        PautaRequest pautaRequest = TestUtils.generateRandom(PautaRequest.class);
+        pautaRequest.setDataHoraInicio(LocalDateTime.now().minusDays(1));
+        assertThrows(BusinessException.class, () -> this.pautaService.cadastrarPauta(pautaRequest)
+                , "Data de início da pauta não pode ser menor que a data atual");
+    }
+
+    @Test
+    public void deveLancarBusinessExceptionQuandoDataHoraInicioForMaiorQueAFInal() {
+        PautaRequest pautaRequest = TestUtils.generateRandom(PautaRequest.class);
+        pautaRequest.setDataHoraInicio(LocalDateTime.now());
+        pautaRequest.setDataHoraFim(LocalDateTime.now().plusDays(1));
+        assertThrows(BusinessException.class, () -> this.pautaService.cadastrarPauta(pautaRequest)
+                , "Data de fim da pauta não pode ser menor que a data de início");
+    }
+
+    @Test
+    public void deveConsultarPautaPorId() {
+        PautaEntity pautaEntity = TestUtils.generateRandom(PautaEntity.class);
+        pautaEntity.setId(1L);
+        when(pautaRepository.findById(1L)).thenReturn(Optional.of(pautaEntity));
+        PautaResponse pautaResponse = this.pautaService.consultarPautaPorId(1L);
+        verify(pautaRepository).findById(1L);
+        assertEquals(1L, pautaResponse.id());
+    }
+
+    @Test
+    public void deveConsultarPautaPorIdLancandoRecursoNaoEncontrado() {
+        when(pautaRepository.findById(1L)).thenReturn(Optional.empty());
+        assertThrows(RecursoNaoEncontradoException.class, () -> this.pautaService.consultarPautaPorId(1L));
+        verify(pautaRepository).findById(1L);
     }
 }
